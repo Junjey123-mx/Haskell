f8495348ba290bfe0a212b3ec926163d45f4c484
Junjey123-mx <verneljosue@gmail.com>
2025-02-06 17:40:15 -0600

    Datos personales.
    Agregue mis datos personales al código para evidenciar qué alumno lo realizo.

--- Haskell_Hello/Practica01/holaMundo.hs
+++ Haskell_Hello/Practica01/holaMundo.hs
@@ -1,2 +1,8 @@
+{-
+   -Datos del Alumno:
+        - Nombre: Vernel Josue Hernández Cáceres.
+        - Carne: 24584.
+        - Curso: Algoritmos y Estructura de datos.
+-}
 main :: IO ()
 main = putStrLn "Hola Mundo desde Haskell"
